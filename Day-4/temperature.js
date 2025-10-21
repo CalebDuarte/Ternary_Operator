94f8506ddc36a4a1802efbbda8c1e4ed25e3221c
CalebDuarte <caleb.jw.0309@gmail.com>
2025-10-21 16:18:01 -0700

    Delete Day-4 directory

--- Day-4/temperature.js
+++
@@ -1,7 +0,0 @@
-// write a js code to test teperature status
-const prompt = require('prompt-sync')();
-
-let temperature = parseInt(prompt("What is the temperature?"));
-
-let result = temperature > 30? "Hot" : "Normal";
-console.log("The temperature is" + result)
--- Day-4/weekend.js
+++
@@ -1,7 +0,0 @@
-// write a js to check if it is the weekend
-const prompt = require('prompt-sync')();
-
-
-let Day = prompt("What day is it  ?");
-let result = (Day ==="Saturday" || Day==="Sunday") ? "Weekend" : "Weekday";
-console.log("It is " + result)
